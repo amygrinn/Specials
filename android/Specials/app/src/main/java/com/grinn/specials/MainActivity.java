5429ae07812b1ba742e1f0f964b0e1db5d111e39
Tyler Grinn <tylergrinn@gmail.com>
2017-07-21 20:16:00 -0400

    first commit

---
+++ android/Specials/app/src/main/java/com/grinn/specials/MainActivity.java
@@ -0,0 +1,42 @@
+package com.grinn.specials;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl("https://specials-a1572.firebaseapp.com/");
+
+        //Enable javascript
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        //Bind webview client so that links will load in the app
+        //myWebView.setWebViewClient(new WebViewClient());
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+}
